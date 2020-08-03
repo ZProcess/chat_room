200f998d0aaf26f69fc97c7edad758fbb2afcc30
93796 <fjqzzjc/77889654>
2020-08-03 17:28:28 +0800

    功能：样式

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,9 +7,51 @@ Page({
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
+    openid:""
   },
+  onChange(event) {
+    wx.showToast({
+      title: `切换到标签 ${event.detail.name}`,
+      icon: 'none',
+    });
+  },
+  onAdd: function () {
+    const db = wx.cloud.database()
+    console.log(this.data)
+    db.collection('user_info').add({
+      data: {
+        nickName: this.data.userInfo.nickName,
+        gender: this.data.userInfo.gender,
+        city: this.data.userInfo.city,
+        province: this.data.userInfo.province,
+        country: this.data.userInfo.country,
+      },
+      success: res => {  
+      },
+      fail: err => {
 
+      }
+    })
+  },
+  onQuery: function() {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    console.log(this.data.openid)
+    db.collection('user_info').where({
+      _openid: this.data.openid
+    }).count({
+      success: res => {
+        if(res.total == 0){
+          this.onAdd();
+        }
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
   onLoad: function() {
     if (!wx.cloud) {
       wx.redirectTo({
@@ -24,11 +66,15 @@ Page({
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
+            lang:"zh_CN",
             success: res => {
+              console.log("----------------");
+              console.log(res.userInfo);
               this.setData({
                 avatarUrl: res.userInfo.avatarUrl,
                 userInfo: res.userInfo
               })
+              this.onQuery();
             }
           })
         }
@@ -54,15 +100,18 @@ Page({
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
         app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
+        this.setData({
+          openid:res.result.openid
         })
+        // wx.navigateTo({
+        //   url: '../userConsole/userConsole',
+        // })
       },
       fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
+        // console.error('[云函数] [login] 调用失败', err)
+        // wx.navigateTo({
+        //   url: '../deployFunctions/deployFunctions',
+        // })
       }
     })
   },
